37b9bfb4aa27d72f21706602d6534484cf5315d0
uwi0 <kakapoweb@gmail.com>
2024-06-24 20:23:55 +0700

    make calcukator work as expected

--- composeApp/src/androidMain/kotlin/com/kakapo/calculator/presentation/App.kt
+++ composeApp/src/androidMain/kotlin/com/kakapo/calculator/presentation/App.kt
@@ -1,3 +1,4 @@
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -31,17 +32,20 @@ private fun CalculatorScreen() {
     val configuration = LocalConfiguration.current
     val uiState by viewModel.uiState.collectAsState()
     val screenWidth = (configuration.screenWidthDp - 5 * 12) / 4
+
     Column(
         modifier = Modifier.fillMaxSize().padding(bottom = 24.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val buttons = Buttons()
+
         Spacer(Modifier.weight(1f))
-        Text(modifier = Modifier.align(Alignment.End).padding(end = 12.dp),text = uiState, style = MaterialTheme.typography.h3)
+        Text(modifier = Modifier.align(Alignment.End).padding(end = 12.dp), text = uiState, style = MaterialTheme.typography.h3)
         Spacer(modifier = Modifier.size(12.dp))
         buttons.buttons.forEach { column ->
             CalculatorButtonRow(screenWidth.dp,column) {
+                Log.d("ButtonClicked", "CalculatorScreen: $it")
                 viewModel.onClicked(it)
             }
         }
@@ -52,7 +56,7 @@ private fun CalculatorScreen() {
 private fun CalculatorButtonRow(size: Dp,buttons: List<String>, onClick: (String) -> Unit) {
     Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
         buttons.forEach { title ->
-            CalculatorButton(size,title, onClick)
+            CalculatorButton(size, title, onClick)
         }
     }
 }
--- shared/src/commonMain/kotlin/viewModel/CalculatorViewModel.kt
+++ shared/src/commonMain/kotlin/viewModel/CalculatorViewModel.kt
@@ -5,6 +5,7 @@ import io.github.aakira.napier.Napier
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import org.koin.core.component.KoinComponent
 
 open class CalculatorViewModel: ViewModel(), KoinComponent {
@@ -12,9 +13,41 @@ open class CalculatorViewModel: ViewModel(), KoinComponent {
     val uiState: StateFlow<String> get() = _uiState.asStateFlow()
     private val _uiState: MutableStateFlow<String> = MutableStateFlow("0")
     
+    private var currentExpression = ""
+    private var number1: String = ""
+    private var number2: String = ""
+
     fun onClicked(button: String) {
-        Napier.d("onclicked button $button")
+        if(button.isOperator()){
+            currentExpression = button
+        } else if(button != "="){
+            _uiState.update { button }
+            if (number1 == "") {
+                number1 = button
+            } else {
+                number2 = button
+            }
+        } else {
+            val result = calculateNumber()
+            _uiState.update { "$result" }
+        }
     }
-    
+
+
+    private fun String.isOperator(): Boolean {
+        return this in listOf("+", "-", "×", "÷")
+    }
+
+    fun calculateNumber(): Int {
+        val converttedNumber1 = number1.toInt()
+        val convertedNumber2 = number2.toInt()
+        return when(currentExpression) {
+            "+" -> converttedNumber1 + convertedNumber2
+            "-" -> converttedNumber1 - convertedNumber2
+            "×" -> converttedNumber1 * convertedNumber2
+            "÷" -> converttedNumber1 / convertedNumber2
+            else -> error("Unsupported operator")
+        }
+    }
 }
 
